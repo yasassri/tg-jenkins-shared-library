bde0d21d3aff9ce7f5a55ffd69109cec77e48cdb
Yasassri <ycrnet@gmail.com>
2018-08-09 22:32:58 +0530

    Update pipeline

--- vars/Pipeline.groovy
+++ vars/Pipeline.groovy
@@ -1,8 +1,6 @@
 #!groovy
-
-import org.wso2.tg.jenkins.util.Common
-
 @Library('shared_library') _
+import org.wso2.tg.jenkins.util.Common
 
 // First we need to validate all the properties, variables for not null or empty in //vars
 
