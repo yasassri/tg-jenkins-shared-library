3ba199439315522a8cf56af3f98f180f84df7d48
Yasassri <ycrnet@gmail.com>
2018-08-10 07:59:55 +0530

    Update pipeline

--- vars/Pipeline.groovy
+++ vars/Pipeline.groovy
@@ -1,11 +1,11 @@
 #!groovy
-import org.wso2.tg.jenkins.util.*
+import org.wso2.tg.jenkins.util.Common
 
 // First we need to validate all the properties, variables for not null or empty in //vars
 
 // The pipeline should resite in a call block
 def call() {
-    echoFunc()
+    Common.echoFunc()
     pipeline {
         agent any
         stages {
