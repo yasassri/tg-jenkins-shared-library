e205d43cf237c8df2ae9426ed90a7931add8c964
Yasassri <ycrnet@gmail.com>
2018-08-10 09:11:04 +0530

    Update pipeline

--- vars/Pipeline.groovy
+++ vars/Pipeline.groovy
@@ -5,7 +5,7 @@ import org.wso2.tg.jenkins.util.Common
 // The pipeline should resite in a call block
 def call() {
 
-    def a = new Common(this)
+    def a = new Common()
     a.echoFunc ()
 
 //    pipeline {
