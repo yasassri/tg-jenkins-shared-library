db629f32429c26f196629f34d7330cf2f9578ab4
Yasassri <ycrnet@gmail.com>
2018-08-10 12:24:35 +0530

    Update pipeline

--- src/org/wso2/tg/jenkins/Log.groovy
+++ src/org/wso2/tg/jenkins/Log.groovy
@@ -20,7 +20,5 @@ class Log implements Serializable {
     private def getTimestamp(Date date = new Date()){
         return date.format('yyyyMMddHHmmss',TimeZone.getTimeZone('GMT')) as String
     }
-
 }
 
-return this
