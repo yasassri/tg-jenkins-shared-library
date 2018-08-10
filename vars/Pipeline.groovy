fb11e6c9ea79ce72fbc117944b2391407394ae58
Yasassri <ycrnet@gmail.com>
2018-08-10 12:58:18 +0530

    Update pipeline

--- vars/Pipeline.groovy
+++ vars/Pipeline.groovy
@@ -1,4 +1,4 @@
-import org.wso2.tg.jenkins.util.Common
+import org.wso2.tg.jenkins.Log
 
 // First we need to validate all the properties, variables for not null or empty in //vars
 
@@ -13,8 +13,8 @@ def call() {
                     steps {
                         script {
                             echo "This is a test"
-                            def a = new Common()
-                            a.echoFunc("Yes")
+                            def a = new Log()
+                            a.info("This is a info")
                         }
                     }
                 }
