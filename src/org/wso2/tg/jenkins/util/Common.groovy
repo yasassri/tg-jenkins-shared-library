a86da499347b42364e18de2340826813661c7261
Yasassri <ycrnet@gmail.com>
2018-08-10 12:40:08 +0530

    Update pipeline

--- src/org/wso2/tg/jenkins/util/Common.groovy
+++ src/org/wso2/tg/jenkins/util/Common.groovy
@@ -2,15 +2,15 @@ package org.wso2.tg.jenkins.util
 
 //import com.cloudbees.groovy.cps.NonCPS
 
-//class Common implements Serializable {
-//    def echoFunc() {
-//        //echo "Test Function 01"
-//    }
-//}
-//@NonCPS
-def echoFunc(msg) {
-    echo "Test Function 01 $msg"
+class Common implements Serializable {
+    def echoFunc(msg) {
+        echo "Test Function 01 $msg"
+    }
 }
+//@NonCPS
+//def echoFunc(msg) {
+//    echo "Test Function 01 $msg"
+//}
 
 //return this
 
