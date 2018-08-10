10b4354841df364928cfa8fd2ea519fd19fbb42f
Yasassri <ycrnet@gmail.com>
2018-08-10 12:59:10 +0530

    Update pipeline

--- src/org/wso2/tg/jenkins/Log.groovy
+++ src/org/wso2/tg/jenkins/Log.groovy
@@ -2,19 +2,22 @@ package org.wso2.tg.jenkins
 
 import org.wso2.tg.jenkins.util.Common
 
-def a = new Common()
+
 
 void info(message) {
+    def a = new Common()
     def time = a.getTimestamp()
     echo "[INFO] : $time : $message"
 }
 
 void error(message) {
+    def a = new Common()
     def time = a.getTimestamp()
     echo "[ERROR] : $time : $message"
 }
 
 void warn(message) {
+    def a = new Common()
     def time = a.getTimestamp()
     echo "[WARN] : $time : $message"
 }
