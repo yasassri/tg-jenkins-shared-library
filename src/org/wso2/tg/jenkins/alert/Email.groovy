45d643fa2ac14cb6a47a3e26d78eb10f038aa78f
Yasassri <ycrnet@gmail.com>
2018-08-14 05:03:10 +0530

    Update 1

--- src/org/wso2/tg/jenkins/alert/Email.groovy
+++ src/org/wso2/tg/jenkins/alert/Email.groovy
@@ -23,13 +23,7 @@ package org.wso2.tg.jenkins.alert
  * @param content body of the Email
  */
 def send(subject,  content) {
-    emailext(to: "${EMAIL_TO_LIST},kasung@wso2.com,lasanthad@wso2.com,yasassri@wso2.com",
+    emailext(to: "yasassri@wso2.com",
             subject: subject,
             body: content, mimeType: 'text/html')
 }
-
-boolean sendEmail(subject, content, attachmentPattern) {
-    return true
-}
-
-
